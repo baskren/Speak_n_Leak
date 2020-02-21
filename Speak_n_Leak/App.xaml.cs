2932fedd369fa39e5c5edc6b5f57857301416539
Benjamin Askren <benjamin.askren@toyota.com>
2020-02-21 07:21:35 -0500

    Initial commit

---
+++ Speak_n_Leak/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Speak_n_Leak
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
